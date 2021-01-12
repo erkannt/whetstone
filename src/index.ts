9239c91158f7202647e53bc389d1c3d6502a2891
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-12 10:37:51 +0000

    add router and pageHandler Middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-import Koa from 'koa';
+import Koa, { Middleware } from 'koa';
+import Router from '@koa/router'
 
-const mainContent = `
+type Page = () => string
+
+const homepage: Page = () => `
     <h1>Whetstone</h1>
     <a href="/login">Login<a>
 `;
+
+const pageHandler = (renderPage: Page): Middleware => (context) => {
+    context.body = renderPage()
+    context.status = 200
+}
+
+const router = new Router()
+
+router.get('/',
+    pageHandler(homepage))
+
 const app = new Koa()
 
-app.use(async context => {
-    context.body = mainContent;
-})
+app.use(router.middleware())
 
-app.listen(8080);
+app.listen(8080)
