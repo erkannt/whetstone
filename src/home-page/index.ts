7c4e4f7a8de0c36c0947085faee52b5f10d6b447
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-12 11:26:23 +0000

    conditional render of login link

--- src/home-page/index.ts
+++ src/home-page/index.ts
@@ -1,10 +1,24 @@
 import { Context } from 'koa'
+import * as O from 'fp-ts/lib/Option'
+import { pipe } from 'fp-ts/lib/function'
 
-const renderMain = (): string => `
-    <h1>Whetstone</h1>
-    <a href="/login">Login<a>
-`
+const renderLogin = (user: O.Option<string>): string => (
+    pipe(
+        user,
+        O.fold(
+            () => '<a href="/login">Login<a>',
+            (_) => '<a href="/logout">Logout<a>',
+        )
+    )
+)
 
 export const homepage = (_: Context): string => (
-    renderMain()
+    pipe(
+        O.none,
+        renderLogin,
+        (body) => `
+            <h1>Whetstone</h1>
+            ${body}
+            `
+    )
 )
