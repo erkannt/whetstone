14387f57d7de89fca713d57b88170750530d24b0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-19 11:05:34 +0000

    improve navbar rendering

--- src/home-page/index.ts
+++ src/home-page/index.ts
@@ -2,24 +2,42 @@ import * as O from 'fp-ts/lib/Option'
 import { pipe } from 'fp-ts/lib/function'
 import { MyContext } from '../context'
 
-const renderLogin = (user: O.Option<string>): string => (
-    pipe(
+const renderNavbar = (user: O.Option<string>): string => {
+    type ViewModel = {
+        login: string,
+        username: string,
+    }
+
+    return pipe(
         user,
-        O.fold(
-            () => '<a href="/login">Login<a>',
-            (_) => '<a href="/logout">Logout<a>',
-        )
+        (u) => (
+            {
+                login: O.fold(
+                    () => '<a href="/login">Login<a>',
+                    (_) => '<a href="/logout">Logout<a>'
+                )(u),
+                username: O.fold(
+                    () => '',
+                    (u) => `${u}`,
+                )(u)
+            }
+        ),
+        (c: ViewModel) => `
+            <nav>
+                ${c.username}
+                ${c.login}
+            </nav
+        `
     )
-)
+}
 
 export const homepage = (ctx: MyContext): string => (
     pipe(
         O.fromNullable(ctx.state.user),
-        renderLogin,
-        (body) => `
+        renderNavbar,
+        (navbar) => `
             <h1>Whetstone</h1>
-            ${body}
-            ${JSON.stringify(ctx.state.user, null, 2)}
+            ${navbar}
             `
     )
 )
