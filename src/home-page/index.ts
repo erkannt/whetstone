67fc502b2d4abccf8a3b00cd2463a1c1257ce801
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-19 11:19:20 +0000

    tidy up homepage/index.ts

--- src/home-page/index.ts
+++ src/home-page/index.ts
@@ -1,43 +1,48 @@
 import * as O from 'fp-ts/lib/Option'
-import { pipe } from 'fp-ts/lib/function'
+import { constant, pipe } from 'fp-ts/lib/function'
 import { MyContext } from '../context'
+import { cons } from 'fp-ts/lib/ReadonlyArray'
 
 const renderNavbar = (user: O.Option<string>): string => {
-    type ViewModel = {
-        login: string,
-        username: string,
-    }
+  type ViewModel = {
+    login: string,
+    username: string,
+  }
 
-    return pipe(
-        user,
-        (u) => (
-            {
-                login: O.fold(
-                    () => '<a href="/login">Login<a>',
-                    (_) => '<a href="/logout">Logout<a>'
-                )(u),
-                username: O.fold(
-                    () => '',
-                    (u) => `${u}`,
-                )(u)
-            }
+  return pipe(
+    user,
+    (u) => (
+      {
+        login: pipe(u,
+          O.fold(
+            constant('<a href="/login">Login<a>'),
+            constant('<a href="/logout">Logout<a>')
+          )
         ),
-        (c: ViewModel) => `
-            <nav>
-                ${c.username}
-                ${c.login}
-            </nav
+        username: pipe(u,
+          O.fold(
+            constant(''),
+            (u) => `${u}`,
+          )
+        )
+      }
+    ),
+    (c: ViewModel) => `
+      <nav>
+        ${c.username}
+        ${c.login}
+      </nav
         `
-    )
+  )
 }
 
 export const homepage = (ctx: MyContext): string => (
-    pipe(
-        O.fromNullable(ctx.state.user),
-        renderNavbar,
-        (navbar) => `
-            <h1>Whetstone</h1>
-            ${navbar}
-            `
-    )
+  pipe(
+    O.fromNullable(ctx.state.user),
+    renderNavbar,
+    (navbar) => `
+      <h1>Whetstone</h1>
+      ${navbar}
+      `
+  )
 )
