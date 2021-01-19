60209a5c377a5f8781a8b85459f43621e3c778be
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-19 09:59:08 +0000

    setup koa-passport with github strat

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
-import Koa, { Middleware } from 'koa';
+import Koa from 'koa';
 import { router } from './router';
+import koaSession from 'koa-session';
+import koaPassport from 'koa-passport';
+import {Strategy as GitHubStrategy} from 'passport-github2';
 
 const app = new Koa()
+app.keys = ['something-secret']
 
-app.use(router().middleware())
+koaPassport
+  .serializeUser((user, done) => done(null, user))
+koaPassport
+  .deserializeUser((obj: undefined, done) => done(null, obj))
+koaPassport
+  .use(new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID ?? 'my-key',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? 'my-secret',
+      callbackURL: "http://127.0.0.1:8080/auth/github/callback"
+    },
+    (_accessToken: any, _refreshToken: any, profile: any, done: any) => { return done(null, profile) }
+    )
+  );
+
+app
+  .use(koaSession({}, app))
+  .use(koaPassport.initialize())
+  .use(koaPassport.session())
+  .use(router().middleware())
 
 app.listen(8080)
