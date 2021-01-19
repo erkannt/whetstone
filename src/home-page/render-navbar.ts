7a4a288ab53136d9ca0ebd0d1d55e1d97b48ac33
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-19 12:35:45 +0000

    reshape render-navbar

--- src/home-page/render-navbar.ts
+++ src/home-page/render-navbar.ts
@@ -1,29 +1,32 @@
 import * as O from 'fp-ts/lib/Option'
 import { constant, pipe } from 'fp-ts/lib/function'
 
+const authLink = (user: O.Option<string>): string => pipe(
+  user,
+  O.fold(
+    constant('<a href="/login">Login<a>'),
+    constant('<a href="/logout">Logout<a>')
+  )
+)
+
+const currentUserDisplay = (user: O.Option<string>): string => pipe(
+  user,
+  O.fold(
+    constant(''),
+    (u) => `${u}`,
+  )
+)
+
 type ViewModel = {
   login: string,
   username: string,
 }
 
 export const renderNavbar = (user: O.Option<string>): string => pipe(
-  user,
-  (u) => (
-    {
-      login: pipe(u,
-        O.fold(
-          constant('<a href="/login">Login<a>'),
-          constant('<a href="/logout">Logout<a>')
-        )
-      ),
-      username: pipe(u,
-        O.fold(
-          constant(''),
-          (u) => `${u}`,
-        )
-      )
-    }
-  ),
+  {
+    login: authLink(user),
+    username: currentUserDisplay(user),
+  },
   (c: ViewModel) => `
       <nav>
         ${c.username}
