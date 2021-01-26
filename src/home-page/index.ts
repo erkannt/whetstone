5599c74379ed1d56ea32781fa60981bfbe3d472a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-01-26 11:46:20 +0000

    lift user into TE

--- src/home-page/index.ts
+++ src/home-page/index.ts
@@ -4,13 +4,15 @@ import { MyContext } from '../context'
 import { renderLogin } from './render-login'
 import { renderUser } from './render-user'
 import * as T from 'fp-ts/lib/Task'
+import * as TE from 'fp-ts/lib/TaskEither'
 import * as E from 'fp-ts/lib/Either'
+import { sequenceS } from 'fp-ts/lib/Apply'
 
 
-const userFromContext = (ctx: MyContext): E.Either<string, User> => pipe(
+const userFromContext = (ctx: MyContext): TE.TaskEither<string, User> => pipe(
   O.fromNullable(ctx.state.user),
-  E.fromOption(constant('not-logged-in')),
-  E.map(
+  TE.fromOption(constant('not-logged-in')),
+  TE.map(
     (u) => ({
       id: u,
       name: 'erkannt',
@@ -31,11 +33,13 @@ export const homepage = (ctx: MyContext): T.Task<string> => (
     {
       user: userFromContext(ctx)
     },
-    (m: Homepage) => `
-      <h1>Whetstone</h1>
-      ${renderUser(m.user)}
-      ${renderLogin(m.user)}
-      `,
-    T.of,
+    sequenceS(T.task),
+    T.map(
+      (m: Homepage) => `
+        <h1>Whetstone</h1>
+        ${renderUser(m.user)}
+        ${renderLogin(m.user)}
+        `,
+    )
   )
 )
